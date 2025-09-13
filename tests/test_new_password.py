46a6282b7b054ed64cf76b34e39ae70f31e14b83
Артем Кр <artik.skam@gmail.com>
2025-09-13 18:20:30 +0200

    add function

--- tests/test_new_password.py
+++ tests/test_new_password.py
@@ -8,6 +8,15 @@ def test_password_characters():
     for char in password:
         assert char in valid_characters
 
+
+def test_password_complexity():
+    """Тест, что пароль содержит букву, цифру и спецсимвол"""
+    password = generate_password(20)
+    letter = any(char in string.ascii_letters for char in password)
+    digit = any(char in string.digits for char in password)
+    punct = any(char in string.punctuation for char in password)
+    assert letter and digit and punct
+
 """
 Допиши еще один тест из предложенных. Или придумай свой.
 Если сможешь написать больше, то будет круто!
